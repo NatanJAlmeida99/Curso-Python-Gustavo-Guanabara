a65206c98acc20a7aff47b5abc8ab173936b66a3
Natan Almeida <natanalmeida158@hotmail.com>
2025-10-06 09:40:57 -0300

    Create desafio113.py

---
+++ mundo_3/desafio113.py
@@ -0,0 +1,28 @@
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print("ERRO: Por favor digite um número inteiro válido")
+        except (KeyboardInterrupt):
+            print("Usuário preferiu não digitar esse número;")
+            return 0
+        else:
+            return n
+
+
+def leiaFloat(msg):
+    while True:
+        try:
+            n = float(input(msg))
+        except (ValueError, TypeError):
+            print("ERRO: por favor, digite um número real válido")
+        except (KeyboardInterrupt):
+            print("Usuário preferiu não digitar esse número")
+            return 0
+        else:
+            return n
+
+n1 = leiaInt("Digite um Inteiro: ")
+n2 = leiaFloat("Digite um Real: ")
+print(f"O valor inteiro digitado foi {n1} e o real foi {n2}")
